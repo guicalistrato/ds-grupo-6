95fe90f58bef079904cdc68e31cf7e0bea0c74cd
guicalistrato <gcnt100@gmail.com>
2026-04-07 13:08:22 -0300

    separei as rotas get e post em funções diferentes

--- app.py
+++ app.py
@@ -22,101 +22,102 @@ def after_request(response):
     return response
 
 # pagina inicial
-@app.route("/", methods=["GET", "POST"])
+@app.get("/")
 @login_required
-def index():
-    if request.method == 'POST':
-        dados = request.get_json()
-        print(dados)
-        if not dados:
-            return {"erro": "Dados não recebidos"}, 400
-        
-        # recebe pergunta e roda função para boole gerar resposta
-        duvida = dados.get('duvida')
-        resposta_boole = run_boole(duvida)
-        print(resposta_boole)
+def index_get():
+    return render_template("index.html")
 
-        # retorna o resultado em forma de json
-        return {"resultado": resposta_boole}, 200
+@app.post("/")
+@login_required
+def index_post():
+    dados = request.get_json()
+    print(dados)
+    if not dados:
+        return {"erro": "Dados não recebidos"}, 400
+    
+    # recebe pergunta e roda função para boole gerar resposta
+    duvida = dados.get('duvida')
+    resposta_boole = run_boole(duvida)
+    print(resposta_boole)
 
-    else:
-        return render_template("index.html")
+    # retorna o resultado em forma de json
+    return {"resultado": resposta_boole}, 200
 
 # página de login
-@app.route('/login', methods=["GET", "POST"])
-def login():
+@app.get('/login')
+def login_get():
     session.clear()
+    return render_template('login.html')
 
-    if request.method == 'POST':
-        # recebe os dados do javascript
-        dados = request.get_json()
+@app.post('/login')
+def login_post():
+    session.clear()
+    # recebe os dados do javascript
+    dados = request.get_json()
+    
+    if not dados:
+        return {"erro": "Dados não recebidos"}, 400 
         
-        if not dados:
-            return {"erro": "Dados não recebidos"}, 400 
-            
-        usuario = dados.get('usuario')
-        senha = dados.get('senha')
+    usuario = dados.get('usuario')
+    senha = dados.get('senha')
 
-        # checa se os dados batem
-        with sqlite3.connect('dados.db') as conn:
-            db = conn.cursor()
-            db.execute("SELECT senha FROM usuarios WHERE usuario = ?", (usuario,))
-            senha_db = db.fetchone()
+    # checa se os dados batem
+    with sqlite3.connect('dados.db') as conn:
+        db = conn.cursor()
+        db.execute("SELECT senha FROM usuarios WHERE usuario = ?", (usuario,))
+        senha_db = db.fetchone()
 
-        if senha_db and check_password_hash(senha_db[0], senha):
-            session["user_id"] = usuario
-            print('ok')
-            return {"redirect": "/"}, 200
-        else:
-            print('nao ok')
-
-    # exibe a pagina
+    if senha_db and check_password_hash(senha_db[0], senha):
+        session["user_id"] = usuario
+        print('ok')
+        return {"redirect": "/"}, 200
     else:
-        return render_template('login.html')
+        print('nao ok')
 
 # página de criar conta
-@app.route('/criar-conta', methods=["GET", "POST"])
-def criar_conta():
+@app.get('/criar-conta')
+def criar_conta_get():
     session.clear()
+    return render_template('criar_conta.html')
 
-    if request.method == 'POST':
-        # recebe os dados pelo json
-        dados = request.get_json()
+@app.post('/criar-conta')
+def criar_conta_post():
+    session.clear()
+    # recebe os dados pelo json
+    dados = request.get_json()
+    
+    if not dados:
+        return "Erro: Dados não recebidos", 400
         
-        if not dados:
-            return "Erro: Dados não recebidos", 400
-            
-        usuario = dados.get('usuario')
-        senha = dados.get('senha')
-        senha_confirma = dados.get('senha_confirma')
+    usuario = dados.get('usuario')
+    senha = dados.get('senha')
+    senha_confirma = dados.get('senha_confirma')
 
-        print(f'DEBUG: usuario : {usuario}, senha : {senha}, senha2 : {senha_confirma}')
+    print(f'DEBUG: usuario : {usuario}, senha : {senha}, senha2 : {senha_confirma}')
 
-        # checa se as senhas sao iguais
-        if senha == senha_confirma:
-            hash = generate_password_hash(senha)
+    # checa se as senhas sao iguais
+    if senha == senha_confirma:
+        hash = generate_password_hash(senha)
 
-            # guarda usuario e senha no banco de dados
-            with sqlite3.connect('dados.db') as conn:
-                db = conn.cursor()
+        # guarda usuario e senha no banco de dados
+        with sqlite3.connect('dados.db') as conn:
+            db = conn.cursor()
 
-                # checa se o usuario ja existe
-                db.execute("SELECT usuario FROM usuarios WHERE usuario = ?", (usuario,))
-                usuario_existente = db.fetchone()
+            # checa se o usuario ja existe
+            db.execute("SELECT usuario FROM usuarios WHERE usuario = ?", (usuario,))
+            usuario_existente = db.fetchone()
 
-                # se sim, retorna o erro
-                if usuario_existente:
-                    return {"erro": "Esse nome de usuário já está em uso."}, 400
+            # se sim, retorna o erro
+            if usuario_existente:
+                return {"erro": "Esse nome de usuário já está em uso."}, 400
 
-                else:
-                    db.execute("INSERT INTO usuarios (usuario, senha) VALUES (?, ?);", (usuario, hash))
-                    conn.commit()
-            
-            # redireciona para a tela de login
-            return {"redirect": "/login"}, 400
+            else:
+                db.execute("INSERT INTO usuarios (usuario, senha) VALUES (?, ?);", (usuario, hash))
+                conn.commit()
+        
+        # redireciona para a tela de login
+        return {"redirect": "/login"}, 400
 
-        # se as senhas forem diferentes, retorna para a parte de criar conta
-        else:
-            return {"redirect": "/criar-conta"}, 200
+    # se as senhas forem diferentes, retorna para a parte de criar conta
     else:
-        return render_template('criar_conta.html')
+        return {"redirect": "/criar-conta"}, 200
